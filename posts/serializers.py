a45df003d508ea63de1ea88fa4cdf4fd49cdbb12
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-09-18 10:52:55 +0000

    create postlist view

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Posts
+from .models import Post
 
 class PostsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -7,12 +7,28 @@ class PostsSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image large than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
-        model = Posts
+        model = Post
         fields = [
-            'id', 'owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title', 'content', 'image', 'is_owner'
+            'id', 'owner', 'profile_id', 'profile_image', 'created_at',
+            'updated_at', 'title', 'content', 'image', 'is_owner', 'image_filter'
         ]
