3a0c5c41fd39e26fbe336b91cef6a9d2bb7014d7
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-09-19 11:03:12 +0000

    add search fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
+
 
 class PostsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -26,9 +31,20 @@ class PostsSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            # print(following)
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'created_at',
-            'updated_at', 'title', 'content', 'image', 'is_owner', 'image_filter'
+            'updated_at', 'title', 'content', 'image', 'is_owner', 'image_filter',
+            'like_id', 'likes_count', 'comments_count',
         ]
